d4d1c11ed8545e379d8f64beb76b661fcaff8645
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-07-29 08:19:00 +0200

    feat: added amount-bar component

---
+++ src/components/ui/amount-bar.tsx
@@ -0,0 +1,23 @@
+export default function AmountBar({
+  amount,
+  max,
+  themeColor,
+}: {
+  amount: number;
+  max: number;
+  themeColor: string;
+}) {
+  const widthPercent = Math.round((amount / max) * 100);
+
+  return (
+    <div className="grid h-8 rounded-sm bg-amber-50 p-1">
+      <div
+        className="inline rounded-sm"
+        style={{
+          backgroundColor: themeColor,
+          width: `${widthPercent}%`,
+        }}
+      />
+    </div>
+  );
+}
