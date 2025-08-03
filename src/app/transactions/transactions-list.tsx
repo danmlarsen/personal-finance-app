cb627724489ac3b56edfe382dae743228d58df50
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-03 10:00:46 +0200

    feat: start implementing pagination menu for transactions

--- src/app/transactions/transactions-list.tsx
+++ src/app/transactions/transactions-list.tsx
@@ -9,6 +9,8 @@ import {
 
 import TransactionTitle from "@/components/transaction-title";
 import { getTransactions } from "@/data/getTransactions";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function TransactionsList({
   transactionName,
@@ -28,34 +30,61 @@ export default async function TransactionsList({
     page,
   });
 
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Recipient / Sender</TableHead>
-          <TableHead>Category</TableHead>
-          <TableHead>Transaction Date</TableHead>
-          <TableHead className="text-end">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
+  const numPages = Math.ceil(totalNumTransactions / 10);
 
-      <TableBody>
-        {transactions.map((transaction, index) => (
-          <TableRow key={index}>
-            <TableCell>
-              <TransactionTitle
-                title={transaction.name}
-                avatarUrl={transaction.avatar}
-              />
-            </TableCell>
-            <TableCell className="w-[120px]">{transaction.category}</TableCell>
-            <TableCell className="w-[120px]">{transaction.date}</TableCell>
-            <TableCell className="w-[200px] text-end">
-              {transaction.amount}
-            </TableCell>
+  return (
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Recipient / Sender</TableHead>
+            <TableHead>Category</TableHead>
+            <TableHead>Transaction Date</TableHead>
+            <TableHead className="text-end">Amount</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+
+        <TableBody>
+          {transactions.map((transaction, index) => (
+            <TableRow key={index}>
+              <TableCell>
+                <TransactionTitle
+                  title={transaction.name}
+                  avatarUrl={transaction.avatar}
+                />
+              </TableCell>
+              <TableCell className="w-[120px]">
+                {transaction.category}
+              </TableCell>
+              <TableCell className="w-[120px]">{transaction.date}</TableCell>
+              <TableCell className="w-[200px] text-end">
+                {transaction.amount}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className="grid grid-cols-[auto_1fr_auto]">
+        <Button variant="outline" asChild>
+          <Link href={`/transactions?page=${page > 1 ? page - 1 : 1}`}>
+            Prev
+          </Link>
+        </Button>
+        <div className="flex justify-center gap-2">
+          {Array.from({ length: numPages }).map((_, idx) => (
+            <Button variant="outline" key={idx} asChild>
+              <Link href={`/transactions?page=${idx + 1}`}>{idx + 1}</Link>
+            </Button>
+          ))}
+        </div>
+        <Button asChild variant="outline">
+          <Link
+            href={`/transactions?page=${page < numPages ? page + 1 : numPages}`}
+          >
+            Next
+          </Link>
+        </Button>
+      </div>
+    </>
   );
 }
