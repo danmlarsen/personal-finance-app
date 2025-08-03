3ed6d69db77b893214e7ff9cf462f6a2e4f52392
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-03 09:31:10 +0200

    refactor: remove unused functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,12 +5,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function uniqueTransactionByName(arr: TTransaction[]) {
-  const seen = new Set<string>();
-  return arr.filter((transaction) => {
-    if (seen.has(transaction.name)) return false;
-    seen.add(transaction.name);
-    return true;
-  });
-}
