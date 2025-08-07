185a34143c99c199f4488177776583a8403661f8
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-07 09:13:36 +0200

    fix: change to more appropriate name

--- src/validation/potFormSchema.ts
+++ src/validation/potFormSchema.ts
@@ -1,6 +1,6 @@
 import z from "zod";
 
-export const addPotFormSchema = z.object({
+export const potFormSchema = z.object({
   potName: z.string().max(30),
   target: z.coerce.number().min(10).max(100000),
   theme: z.string(),
