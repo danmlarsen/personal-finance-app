6b631edf6f578b4d274bd283ad7d107014fd8d29
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-07 09:14:00 +0200

    feat: add more actions for pots

--- src/app/pots/actions.ts
+++ src/app/pots/actions.ts
@@ -1,17 +1,15 @@
 "use server";
 
 import { db } from "@/db";
-import { potsTable } from "@/db/schema";
-import { addPotFormSchema } from "@/validation/addPotFormSchema";
-import { InferInsertModel } from "drizzle-orm";
+import { balanceTable, potsTable } from "@/db/schema";
+import { potFormSchema } from "@/validation/potFormSchema";
+import { eq, InferInsertModel, sql } from "drizzle-orm";
 import z from "zod";
 
-export async function createPot({
-  data,
-}: {
-  data: Omit<InferInsertModel<typeof potsTable>, "total">;
-}) {
-  const validation = z.safeParse(addPotFormSchema, data);
+export async function createPot(
+  data: Omit<InferInsertModel<typeof potsTable>, "total">,
+) {
+  const validation = z.safeParse(potFormSchema, data);
 
   if (!validation.success) {
     return {
@@ -20,8 +18,74 @@ export async function createPot({
     };
   }
 
-  db.insert(potsTable).values({
+  await db.insert(potsTable).values({
     ...data,
     total: "0",
+  });
+}
+
+export async function editPot(
+  id: number,
+  data: Omit<InferInsertModel<typeof potsTable>, "total">,
+) {
+  const validation = z.safeParse(potFormSchema, data);
+
+  if (!validation.success) {
+    return {
+      error: true,
+      message: validation.error.issues[0].message ?? "An error occured",
+    };
+  }
+
+  await db
+    .update(potsTable)
+    .set({
+      ...data,
+    })
+    .where(eq(potsTable.id, id));
+}
+
+export async function deletePot(id: number) {
+  await db.transaction(async (tx) => {
+    const [data] = await tx
+      .select({ total: potsTable.total })
+      .from(potsTable)
+      .where(eq(potsTable.id, id));
+
+    if (!data) {
+      return {
+        error: true,
+        message: "Unable to fetch pot data",
+      };
+    }
+
+    await tx
+      .update(balanceTable)
+      .set({ current: sql`${balanceTable.current} + ${data.total}` });
+    await tx.delete(potsTable).where(eq(potsTable.id, id));
+  });
+}
+
+export async function depositPot(id: number, amount: number) {
+  await db.transaction(async (tx) => {
+    await tx
+      .update(balanceTable)
+      .set({ current: sql`${balanceTable.current} - ${amount}` });
+    await tx
+      .update(potsTable)
+      .set({ total: sql`${potsTable.total} + ${amount}` })
+      .where(eq(potsTable.id, id));
+  });
+}
+
+export async function withdrawPot(id: number, amount: number) {
+  await db.transaction(async (tx) => {
+    await tx
+      .update(balanceTable)
+      .set({ current: sql`${balanceTable.current} + ${amount}` });
+    await tx
+      .update(potsTable)
+      .set({ total: sql`${potsTable.total} - ${amount}` })
+      .where(eq(potsTable.id, id));
   });
 }
