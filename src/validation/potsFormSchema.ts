976da970403371a777285fe3935972749037d200
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-09 12:18:59 +0200

    fix: remove theme issue message

--- src/validation/potsFormSchema.ts
+++ src/validation/potsFormSchema.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const potsFormSchema = z.object({
   name: z.string().max(30, "Maximum 30 characters"),
   target: z.coerce.number().positive("Only positive numbers"),
-  theme: z.string("Something"),
+  theme: z.string(),
 });
