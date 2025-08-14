df2a23c07b3549e0a1f16262f8635e5e63dbcf09
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-14 08:04:20 +0200

    feat: add overview components

--- src/app/(logged-in)/(home)/page.tsx
+++ src/app/(logged-in)/(home)/page.tsx
@@ -1,10 +1,18 @@
 import BalanceOverview from "./balance-overview";
+import BudgetsOverview from "./budgets-overview";
+import PotsOverview from "./pots-overview";
+import RecurringBillsOverview from "./recurring-bills-overview";
+import TransactionsOverview from "./transactions-overview";
 
 export default function Home() {
   return (
-    <div>
+    <div className="space-y-6">
       <h1>Overview</h1>
       <BalanceOverview />
+      <PotsOverview />
+      <TransactionsOverview />
+      <BudgetsOverview />
+      <RecurringBillsOverview />
     </div>
   );
 }
