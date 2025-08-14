ce164a5ec6ee0aa54082481a69acd5642aeab288
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-14 07:48:08 +0200

    feat: budgets-overview skeleton

---
+++ src/app/(logged-in)/(home)/budgets-overview.tsx
@@ -0,0 +1,43 @@
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getBudgets } from "@/data/getBudgets";
+import Link from "next/link";
+import BudgetsPieChart from "../budgets/budgets-pie-chart";
+
+export default async function BudgetsOverview() {
+  const session = await auth();
+  if (!session?.user?.id) return null;
+
+  const userId = Number(session.user.id);
+  const budgets = await getBudgets(userId);
+
+  return (
+    <Card>
+      <CardHeader className="flex items-center justify-between">
+        <CardTitle>Budgets</CardTitle>
+        <Button asChild>
+          <Link href="/budgets">See Details</Link>
+        </Button>
+      </CardHeader>
+      <CardContent className="grid grid-cols-[1fr_101px]">
+        <BudgetsPieChart budgets={budgets} />
+        <ul className="grid gap-4">
+          {budgets.slice(0, 4).map((budget) => (
+            <li
+              key={budget.id}
+              className="grid grid-cols-[auto_1fr] grid-rows-2 gap-x-4 text-xs"
+            >
+              <div
+                className="row-span-2 w-1 rounded-md"
+                style={{ backgroundColor: budget.theme }}
+              />
+              <p>{budget.name}</p>
+              <p>{budget.maximum}</p>
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  );
+}
