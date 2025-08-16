fc793df2ceb53313bea3d351f403da40574b03c0
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-16 14:25:25 +0200

    refactor: use utility fn to get bill summary

--- src/app/(logged-in)/(home)/recurring-bills-overview.tsx
+++ src/app/(logged-in)/(home)/recurring-bills-overview.tsx
@@ -2,46 +2,16 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getRecurringBills } from "@/data/getRecurringBills";
 import { themeColors } from "@/data/themeColors";
-import { isPast, isSameMonth } from "date-fns";
+import { getRecurringBillsSummary } from "@/lib/utils";
 import Link from "next/link";
 
 export default async function RecurringBillsOverview() {
   const recurringBills = await getRecurringBills();
 
-  const today = new Date(2024, 7, 12);
-
-  const paidBills = recurringBills.filter((transaction) => {
-    const transactionDate = new Date(transaction.created_at);
-    return isSameMonth(today, transactionDate) && isPast(transactionDate);
-  });
-  const paidBillsAmount = paidBills.reduce(
-    (total, transaction) => Math.abs(Number(transaction.amount)) + total,
-    0,
+  const billsSummary = getRecurringBillsSummary(
+    recurringBills,
+    new Date(2024, 7, 19).toString(),
   );
-
-  const totalUpcoming = recurringBills.filter((transaction) => {
-    const transactionDate = new Date(transaction.created_at);
-    return transactionDate.getDate() > today.getDate();
-  });
-  const totalUpcomingAmount = totalUpcoming.reduce(
-    (total, transaction) => Math.abs(Number(transaction.amount)) + total,
-    0,
-  );
-
-  const recurringBillsRows = [
-    {
-      title: "Paid Bills",
-      amount: paidBillsAmount,
-    },
-    {
-      title: "Total Upcoming",
-      amount: totalUpcomingAmount,
-    },
-    {
-      title: "Due Soon",
-      amount: 123,
-    },
-  ];
 
   return (
     <Card>
@@ -53,14 +23,14 @@ export default async function RecurringBillsOverview() {
       </CardHeader>
       <CardContent>
         <ul className="space-y-4">
-          {recurringBillsRows.map((bill, idx) => (
+          {billsSummary.map((bill, idx) => (
             <li
               key={idx}
               className="bg-beige-100 flex items-center justify-between rounded-md border-l-4 px-4 py-5"
               style={{ borderLeftColor: themeColors[idx].hex }}
             >
               <p>{bill.title}</p>
-              <p>{bill.amount.toFixed(2)}</p>
+              <p>${bill.totalSum.toFixed(2)}</p>
             </li>
           ))}
         </ul>
