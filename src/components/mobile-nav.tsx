ded1d0e4c14e97e2e3d307bd1ee2d76df4842fc7
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-16 22:28:37 +0200

    fix: add margin to adjust for the mobile nav

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -2,8 +2,11 @@ import Navigation from "./navigation";
 
 export default function MobileNav() {
   return (
-    <div className="bg-grey-900 fixed right-0 bottom-0 left-0 rounded-t-xl px-4 pt-2 text-white sm:px-10 lg:hidden">
-      <Navigation />
-    </div>
+    <>
+      <div className="mt-[74px] lg:hidden" />
+      <div className="bg-grey-900 fixed right-0 bottom-0 left-0 rounded-t-xl px-4 pt-2 text-white sm:px-10 lg:hidden">
+        <Navigation />
+      </div>
+    </>
   );
 }
