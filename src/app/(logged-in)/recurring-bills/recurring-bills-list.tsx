b6bff5ce358c7a9849e2262a439835b916d03f3f
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-17 15:57:51 +0200

    style: recurring bills list should be responsive on mobile

--- src/app/(logged-in)/recurring-bills/recurring-bills-list.tsx
+++ src/app/(logged-in)/recurring-bills/recurring-bills-list.tsx
@@ -10,50 +10,68 @@ import {
 
 import { type TRecurringBill } from "@/data/getRecurringBills";
 import { format } from "date-fns";
+import Image from "next/image";
 
 export default function RecurringBillsList({
   recurringBills,
 }: {
   recurringBills: TRecurringBill[];
 }) {
-  // const isMobile = useMediaQuery("(max-width: 600px)");
-
-  // if (isMobile) {
-  //   return (
-  //     <ul>
-  //       {recurringBills.map((bill, idx) => (
-  //         <li key={`${bill.name}_${idx}`}>{bill.name}</li>
-  //       ))}
-  //     </ul>
-  //   );
-  // }
-
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Bill Title</TableHead>
-          <TableHead>Due Date</TableHead>
-          <TableHead className="text-end">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {recurringBills.map((bill, idx) => {
-          return (
-            <TableRow key={`${bill.name}_${idx}`}>
-              <TableCell>
-                <TransactionTitle title={bill.name} avatarUrl={bill.avatar} />
-              </TableCell>
-              <TableCell className="w-[120px]">
-                Monthly - {format(bill.created_at, "do")}
-              </TableCell>
-              <TableCell className="w-[100px] text-end">
+    <div className="@container">
+      <ul className="space-y-8 @md:hidden">
+        {recurringBills.map((bill) => (
+          <li key={bill.id} className="space-y-2">
+            <div className="flex items-center gap-4 font-bold">
+              <Image
+                src={bill.avatar}
+                alt={bill.name}
+                width={40}
+                height={40}
+                className="size-10 rounded-full"
+              />
+              <p>{bill.name}</p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p>Monthly - {format(bill.created_at, "do")}</p>
+              <p className="font-bold">
                 ${Math.abs(Number(bill.amount)).toFixed(2)}
-              </TableCell>
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div className="hidden @md:block">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Bill Title</TableHead>
+              <TableHead>Due Date</TableHead>
+              <TableHead className="text-end">Amount</TableHead>
             </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+          </TableHeader>
+          <TableBody>
+            {recurringBills.map((bill) => {
+              return (
+                <TableRow key={bill.id}>
+                  <TableCell>
+                    <TransactionTitle
+                      title={bill.name}
+                      avatarUrl={bill.avatar}
+                    />
+                  </TableCell>
+                  <TableCell className="w-[120px]">
+                    Monthly - {format(bill.created_at, "do")}
+                  </TableCell>
+                  <TableCell className="w-[100px] text-end">
+                    ${Math.abs(Number(bill.amount)).toFixed(2)}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
   );
 }
