21f0604ba9ad83e70f6d72d75e5a4654f36f1ac5
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-18 09:18:05 +0200

    feat: should be able to override classes on amount-bar

--- src/components/ui/amount-bar.tsx
+++ src/components/ui/amount-bar.tsx
@@ -1,16 +1,24 @@
+import { cn } from "@/lib/utils";
+import { ComponentProps } from "react";
+
 export default function AmountBar({
   amount,
   max,
   themeColor,
+  className,
+  ...props
 }: {
   amount: number;
   max: number;
   themeColor: string;
-}) {
+} & ComponentProps<"div">) {
   const widthPercent = Math.min(Math.round((amount / max) * 100), 100);
 
   return (
-    <div className="bg-beige-100 grid h-8 rounded-sm p-1">
+    <div
+      className={cn("bg-beige-100 grid h-8 rounded-sm", className)}
+      {...props}
+    >
       <div
         className="inline rounded-sm"
         style={{
