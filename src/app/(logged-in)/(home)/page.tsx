b3162ea865ef3d1eb81159c2a0791ffecf7d409d
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-21 10:45:32 +0200

    style: layout adjustments on home & budget pages

--- src/app/(logged-in)/(home)/page.tsx
+++ src/app/(logged-in)/(home)/page.tsx
@@ -6,13 +6,19 @@ import TransactionsOverview from "./transactions-overview";
 
 export default function Home() {
   return (
-    <div className="space-y-8">
+    <div className="@container space-y-8">
       <h1 className="text-3xl font-bold">Overview</h1>
       <BalanceOverview />
-      <PotsOverview />
-      <TransactionsOverview />
-      <BudgetsOverview />
-      <RecurringBillsOverview />
+      <div className="grid gap-6 @5xl:grid-cols-[7fr_5fr]">
+        <div className="grid gap-6">
+          <PotsOverview />
+          <TransactionsOverview />
+        </div>
+        <div className="grid gap-6">
+          <BudgetsOverview />
+          <RecurringBillsOverview />
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/(logged-in)/budgets/page.tsx
+++ src/app/(logged-in)/budgets/page.tsx
@@ -27,7 +27,7 @@ export default async function BudgetsPage() {
           <h1 className="text-3xl font-bold">Budgets</h1>
           <AddNewBudgetButton categories={categories} />
         </div>
-        <div className="grid items-start gap-6 @5xl:grid-cols-[428px_1fr]">
+        <div className="grid items-start gap-6 @4xl:grid-cols-[428px_1fr]">
           <Card>
             <CardContent>
               <BudgetsSummary budgets={budgets} />
