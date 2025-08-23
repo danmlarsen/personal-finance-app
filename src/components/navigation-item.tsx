e197fa10f7a3007514435eba27d088eeb802095f
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-23 07:59:57 +0200

    style: display current route in navigation & adjust hover colors

---
+++ src/components/navigation-item.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function NavigationItem({
+  title,
+  url,
+  icon,
+  variant,
+}: {
+  title: string;
+  url: string;
+  icon: React.ReactNode;
+  variant?: "sm" | "lg";
+}) {
+  const pathname = usePathname();
+
+  return (
+    <li key={title}>
+      <Button
+        asChild
+        className={cn(
+          "bg-grey-900 hover:bg-grey-900 hover:text-grey-100 text-grey-300 h-11 w-full flex-col rounded-none rounded-t-md sm:h-16 lg:flex-row lg:justify-start lg:gap-4 lg:rounded-none lg:rounded-r-xl lg:pl-8",
+          variant === "sm" && "lg:justify-center lg:pl-0",
+          pathname === url &&
+            "bg-beige-100 text-grey-900 hover:bg-beige-100 hover:text-grey-900 border-b-4 border-green-500 lg:border-b-0 lg:border-l-4",
+        )}
+        size="nav"
+      >
+        <Link href={url}>
+          <span className={cn("", pathname === url && "text-green-500")}>
+            {icon}
+          </span>
+          {variant === "lg" && <span className="hidden sm:block">{title}</span>}
+        </Link>
+      </Button>
+    </li>
+  );
+}
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,7 +1,5 @@
-import { Button } from "./ui/button";
-import Link from "next/link";
 import { navData } from "@/data/nav-data";
-import { cn } from "@/lib/utils";
+import NavigationItem from "./navigation-item";
 
 export default function Navigation({
   variant = "lg",
@@ -11,27 +9,15 @@ export default function Navigation({
   return (
     <nav>
       <ul className="grid grid-cols-5 sm:gap-10 lg:block">
-        {navData.map((link) => {
-          const Icon = link.icon;
-
+        {navData.map(({ icon: Icon, ...link }) => {
           return (
-            <li key={link.title}>
-              <Button
-                asChild
-                className={cn(
-                  "bg-grey-900 h-11 w-full flex-col sm:h-16 lg:flex-row lg:justify-start lg:gap-4 lg:rounded-none lg:rounded-r-xl lg:pl-8",
-                  variant === "sm" && "lg:justify-center lg:pl-0",
-                )}
-                size="nav"
-              >
-                <Link href={link.url}>
-                  <Icon />
-                  {variant === "lg" && (
-                    <span className="hidden sm:block">{link.title}</span>
-                  )}
-                </Link>
-              </Button>
-            </li>
+            <NavigationItem
+              key={link.title}
+              title={link.title}
+              url={link.url}
+              icon={<Icon />}
+              variant={variant}
+            />
           );
         })}
       </ul>
