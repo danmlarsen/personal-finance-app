48a14a7a427f900ea84dcc4dc2f964e7884857ff
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-23 20:34:39 +0200

    feat: add suspense for loading several async components on home page

--- src/app/(logged-in)/(home)/page.tsx
+++ src/app/(logged-in)/(home)/page.tsx
@@ -1,22 +1,34 @@
+import { Suspense } from "react";
 import BalanceOverview from "./balance-overview";
 import BudgetsOverview from "./budgets-overview";
 import PotsOverview from "./pots-overview";
 import RecurringBillsOverview from "./recurring-bills-overview";
 import TransactionsOverview from "./transactions-overview";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
   return (
     <div className="@container space-y-8">
       <h1 className="text-3xl font-bold">Overview</h1>
-      <BalanceOverview />
+      <Suspense fallback={<Skeleton className="h-60" />}>
+        <BalanceOverview />
+      </Suspense>
       <div className="grid gap-6 @5xl:grid-cols-[7fr_5fr]">
         <div className="grid gap-6">
-          <PotsOverview />
-          <TransactionsOverview />
+          <Suspense fallback={<Skeleton className="h-60" />}>
+            <PotsOverview />
+          </Suspense>
+          <Suspense fallback={<Skeleton className="h-[40rem]" />}>
+            <TransactionsOverview />
+          </Suspense>
         </div>
         <div className="grid gap-6">
-          <BudgetsOverview />
-          <RecurringBillsOverview />
+          <Suspense fallback={<Skeleton className="h-60" />}>
+            <BudgetsOverview />
+          </Suspense>
+          <Suspense fallback={<Skeleton className="h-[40rem]" />}>
+            <RecurringBillsOverview />
+          </Suspense>
         </div>
       </div>
     </div>
