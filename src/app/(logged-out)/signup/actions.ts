18fbeef8b58c05de3b4d04ea50420f7579581ed3
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-08-25 10:19:36 +0200

    feat: add placeholder pots & budgets for new users

--- src/app/(logged-out)/signup/actions.ts
+++ src/app/(logged-out)/signup/actions.ts
@@ -4,7 +4,63 @@ import { signupFormSchema } from "@/validation/signupFormSchema";
 import z from "zod";
 import { hash } from "bcryptjs";
 import { db } from "@/db";
-import { balanceTable, usersTable } from "@/db/schema";
+import { balanceTable, budgetsTable, potsTable, usersTable } from "@/db/schema";
+
+const PLACEHOLDER_BUDGETS = [
+  {
+    categoryId: 7,
+    maximum: "50.0",
+    theme: "#277C78",
+  },
+  {
+    categoryId: 12,
+    maximum: "750.0",
+    theme: "#82C9D7",
+  },
+  {
+    categoryId: 5,
+    maximum: "75.0",
+    theme: "#F2CDAC",
+  },
+  {
+    categoryId: 10,
+    maximum: "100.0",
+    theme: "#626070",
+  },
+];
+
+const PLACEHOLDER_POTS = [
+  {
+    name: "Savings",
+    target: "2000.0",
+    total: "159.0",
+    theme: "#277C78",
+  },
+  {
+    name: "Concert Ticket",
+    target: "150.0",
+    total: "110.0",
+    theme: "#626070",
+  },
+  {
+    name: "Gift",
+    target: "150.0",
+    total: "110.0",
+    theme: "#82C9D7",
+  },
+  {
+    name: "New Laptop",
+    target: "1000.0",
+    total: "10.0",
+    theme: "#F2CDAC",
+  },
+  {
+    name: "Holiday",
+    target: "1440.0",
+    total: "531.0",
+    theme: "#826CB0",
+  },
+];
 
 export async function registerUser({
   data,
@@ -34,12 +90,31 @@ export async function registerUser({
         })
         .returning();
 
-      await tx.insert(balanceTable).values({
-        current: "5000",
-        income: "3814.25",
-        expenses: "1700.5",
-        userId: user.id,
-      });
+      await Promise.all([
+        tx.insert(balanceTable).values({
+          current: "5000",
+          income: "3814.25",
+          expenses: "1700.5",
+          userId: user.id,
+        }),
+
+        //
+        // Create placeholder budgets and pots for new user
+        //
+        tx.insert(budgetsTable).values(
+          PLACEHOLDER_BUDGETS.map((budget) => ({
+            ...budget,
+            userId: user.id,
+          })),
+        ),
+
+        tx.insert(potsTable).values(
+          PLACEHOLDER_POTS.map((pot) => ({
+            ...pot,
+            userId: user.id,
+          })),
+        ),
+      ]);
     });
 
     return {
